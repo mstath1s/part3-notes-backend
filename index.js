8e1cc7d4af2fe8e13c7383a89f2edf7b87e5c265
Meletis <meletis.stathis@cuberm.com>
2022-11-02 20:34:04 +0200

    3.6 Error handling and duplication check

--- index.js
+++ index.js
@@ -3,92 +3,99 @@ const express = require('express')
 const app = express()
 
 let persons = [
-  { 
+  {
     "id": 1,
-    "name": "Arto Hellas", 
+    "name": "Arto Hellas",
     "number": "040-123456"
   },
-  { 
+  {
     "id": 2,
-    "name": "Ada Lovelace", 
+    "name": "Ada Lovelace",
     "number": "39-44-5323523"
   },
-  { 
+  {
     "id": 3,
-    "name": "Dan Abramov", 
+    "name": "Dan Abramov",
     "number": "12-43-234345"
   },
-  { 
+  {
     "id": 4,
-    "name": "Mary Poppendieck", 
+    "name": "Mary Poppendieck",
     "number": "39-23-6423122"
   }
 ]
 const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  
-  app.use(express.json())
 
-  app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
-  })
-  
-  const randomId = () => {
-    const rndid = Math.random() * 100000000
-    console.log(rndid)
-    return Math.floor(rndid)
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+const randomId = () => {
+  const rndid = Math.random() * 100000000
+  console.log(rndid)
+  return Math.floor(rndid)
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'Name or number is missing '
+    })
   }
-  
-  app.post('/api/persons', (request, response) => {
-    const body = request.body
-  
-    if (!body.name) {
-      return response.status(400).json({ 
-        error: 'content missing' 
-      })
-    }
-  
-    const person = {
-      name: body.name,
-      number: body.number || '00-00-00000',
-      id: randomId(),
-    }
-  
-    persons = persons.concat(person)
-    response.json(person)
-  })
 
-  app.get('/api/persons/info', (req, res) => {
-    const infoNote = {
-      content: 'Phonebook',
-      phoneBook: persons.length,
-      date: new Date()
-    }
-   const infoString = infoNote.content +" has info for "+  infoNote.phoneBook + " people <p></p> the date is: "+ infoNote.date
-    console.log( infoString)
-    res.send(infoString)
-  })
-  
-  app.get('/api/persons', (req, res) => {
-    res.json(persons)
-  })
-  
-  app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-  
-    response.status(204).end()
-  })
-  
-  app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-  
-    if (person) {
-      response.json(person)
-    } else {
-      response.status(404).end()
-    }
-  })
+  const personExists = persons.find(person => person.name === body.name)
+  if (personExists) {
+    return response.status(400).json({
+      error: 'name already exists '
+    })
+  }
+
+  const person = {
+    id: randomId(),
+    name: body.name,
+    number: body.number
+  }
+
+  persons = persons.concat(person)
+  response.json(person)
+})
+
+app.get('/api/persons/info', (req, res) => {
+  const infoNote = {
+    content: 'Phonebook',
+    phoneBook: persons.length,
+    date: new Date()
+  }
+  const infoString = infoNote.content + " has info for " + infoNote.phoneBook + " people <p></p> the date is: " + infoNote.date
+  console.log(infoString)
+  res.send(infoString)
+})
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
